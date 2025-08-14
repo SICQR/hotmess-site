f998a5d9ab79565ff8746752b5b8b034c9708909
SICQR <151458843+SICQR@users.noreply.github.com>
2025-08-14 04:03:45 +0100

    feat: add /radio player and /shop redirect

---
+++ src/pages/shop.jsx
@@ -0,0 +1,9 @@
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: 'https://1e0297-a4.myshopify.com',
+      permanent: false,
+    },
+  }
+}
+export default function Shop() { return null }
